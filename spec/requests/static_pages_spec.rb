0d524974da3240d6b8140db3e4d505f492b448bd
Vicente Silveira Inácio <silveira.vicente@gmail.com>
2015-08-18 21:08:16 -0300

    Changing routes and tests.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,19 +1,46 @@
 require_relative '../spec_helper'
+require_relative '../support/utilities'
 
 RSpec.describe "Static Pages" do
 
+	subject { page }
+
 	describe "Home Page" do
-		it "should have the content Home" do
-			visit '/static_pages/home'
-			page.should have_title('Evaluation Test Project for R.D. | Home')
-		end
+
+		before { visit root_path }
+
+		it { should have_title(full_title('Home')) }
+
 	end
 
 	describe "About Page" do
-		it "should have the content About" do
-			visit '/static_pages/about'
-			page.should have_title('Evaluation Test Project for R.D. | About')
-		end
+
+		before { visit about_path }
+
+		it { should have_title(full_title('About')) }
+
 	end
+
+	describe "should get home" do
+		it "should get Home" do
+	    get root_path
+	    assert_response :success
+  	end
+  end
+
+  describe "should get about" do
+		it "should get About" do
+	    get about_path
+	    assert_response :success
+  	end
+  end
+
+  it "should have the right links on the layout" do
+  	visit root_path
+  	click_link "About"
+  	page.should have_title(full_title('About'))
+  	click_link "Home"
+  	page.should have_title(full_title('Home'))
+  end
 
 end
