d5597d88a4c0a50540ec18c7d98e0d14b92db57d
Vicente Silveira Inácio <silveira.vicente@gmail.com>
2015-08-26 15:58:58 -0300

    Correct failing test

--- spec/requests/leads_pages_spec.rb
+++ spec/requests/leads_pages_spec.rb
@@ -1,10 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe "LeadsPages", type: :request do
-  describe "GET /leads_pages" do
-    it "works! (now write some real specs)" do
-      get leads_new_path
-      expect(response).to have_http_status(200)
-    end
-  end
 end
