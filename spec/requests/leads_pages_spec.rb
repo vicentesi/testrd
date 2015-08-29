40ce49167de51285874eb33823e4b6aefe27cc81
Vicente Silveira Inácio <silveira.vicente@gmail.com>
2015-08-29 19:21:38 -0300

    Adding more tests

--- spec/requests/leads_pages_spec.rb
+++ spec/requests/leads_pages_spec.rb
@@ -2,6 +2,60 @@ require_relative '../spec_helper'
 
 RSpec.describe "LeadsPages", type: :request do
 
+  describe "leads" do
+    subject { page }
+    before { visit leads_path }
+
+    it "show leads page" do
+      expect(page).to have_content "All Leads"
+    end
+
+    it "create a lead" do
+      click_link "New Lead"
+      expect(page).to have_content "New Lead"
+    end
+
+    it "delete a lead" do
+      click_link "New Lead"
+      fill_in 'Name', with: "foo"
+      fill_in 'Last name', with: "bar"
+      fill_in 'Email', with: "test@email.com"
+      fill_in 'Company', with: "co"
+      fill_in 'Job title', with: "555-5555"
+      fill_in 'Phone', with: "555-5555"
+      fill_in 'Website', with: "site"
+
+      click_button "Save Lead"
+      click_link "Delete"
+    end
+
+    it "integrate a lead" do
+      click_link "New Lead"
+      fill_in 'Name', with: "foo"
+      fill_in 'Last name', with: "bar"
+      fill_in 'Email', with: "test@email.com"
+      fill_in 'Company', with: "co"
+      fill_in 'Job title', with: "555-5555"
+      fill_in 'Phone', with: "555-5555"
+      fill_in 'Website', with: "site"
+      click_button "Save Lead"
+
+      click_link "Integrate"
+      expect(page).to have_content "Salesforce"
+    end
+
+    it "switch to home page" do
+      click_link "Home"
+      expect(page).to have_content "Evaluation Test Project"
+    end
+
+    it "switch to about page" do
+      click_link "About"
+      expect(page).to have_content "About"
+    end
+
+  end
+
   describe "root" do
     subject { page }
     before { visit root_path }
@@ -35,19 +89,14 @@ RSpec.describe "LeadsPages", type: :request do
       expect(page).to have_content "All Leads"
     end
 
-  end
-
-  describe "leads" do
-    subject { page }
-    before { visit leads_path }
-
-    it "show leads page" do
-      expect(page).to have_content "All Leads"
+    it "switch to about page" do
+      click_link "About"
+      expect(page).to have_content "About"
     end
 
-    it "create a lead" do
-      click_link "New Lead"
-      expect(page).to have_content "New Lead"
+    it "switch to leads page" do
+      click_link "Leads"
+      expect(page).to have_content "All Leads"
     end
 
   end
@@ -59,6 +108,31 @@ RSpec.describe "LeadsPages", type: :request do
     it "show about" do
       expect(page).to have_content "About"
     end
-  end
 
+    it "redirect to testrd github" do
+      href = "https://github.com/vicentesi/testrd"
+      expect(page).to have_selector "a[href='#{href}']", text: "RD Test App"
+    end
+
+    it "redirect to testrd_gem github" do
+      href = "https://github.com/vicentesi/testrd_gem"
+      expect(page).to have_selector "a[href='#{href}']", text: "RD Test Gem"
+    end
+
+    it "redirect to linkedin profile" do
+      href = "https://br.linkedin.com/pub/vicente-silveira-in%C3%A1cio/25/734/588"
+      expect(page).to have_selector "a[href='#{href}']", text: "Vicente Silveira Inácio"
+    end
+
+    it "switch to home page" do
+      click_link "Home"
+      expect(page).to have_content "Evaluation Test Project"
+    end
+
+    it "switch to leads page" do
+      click_link "Leads"
+      expect(page).to have_content "All Leads"
+    end
+
+  end
 end
